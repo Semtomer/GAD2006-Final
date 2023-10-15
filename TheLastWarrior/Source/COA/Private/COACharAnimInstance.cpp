852b7e892f29f186bf86fec8cec933d640e07e8c
Semtomer <eyisa_51@hotmail.com>
2023-10-15 20:57:16 +0300

    Reuploaded the project because the last version of the project had uncompiled shaders.

--- TheLastWarrior/Source/COA/Private/COACharAnimInstance.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "COACharAnimInstance.h"
-
-void UCOACharAnimInstance::ChangeAnimState_Implementation(ECharAnimState NewState)
-{
-	Animstate = NewState;
-}
